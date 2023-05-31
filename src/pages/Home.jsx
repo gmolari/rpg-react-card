074cc4a6254317768880526160da7312c8395754
Guilherme Molari <yguilz123@gmail.com>
2023-05-31 20:57:00 +0000

    started home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import { useContext } from "react"
 import { Context } from "../context/Context";
+import "./home.sass"
 
 export default function Home(){
     const {alo} = useContext(Context);
 
     return (
-        <>
-        
-        </>
+        <section className="section-home">
+            <h2> CARD RPG MAKER </h2>
+            <h4> In this page, you can easily create a D&#38;D sheet, faster and pratice!</h4>
+            <h4> 
+                Just click on Character button above, and be happy!
+            </h4>
+        </section>
     )
 }
